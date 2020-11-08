9197197916f02bd5ca9cd83dc71f99d00a13444b
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-11-08 21:08:37 +0200

    new file:   assets/bundle.js modified:   assets/css/main.css modified:   index.html

---
+++ assets/bundle.js
@@ -0,0 +1,11 @@
+const body = document.querySelector('body');
+
+document.addEventListener('mousedown', () => {
+  body.classList.add('clicking');
+});
+
+document.addEventListener('keydown', e => {
+  if(e.key === 'Tab') {
+    body.classList.remove('clicking');
+  }
+});
