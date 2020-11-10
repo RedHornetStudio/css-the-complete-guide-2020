0fe93ace3358da2bab112a8f7b07d7c980cdddda
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-11-10 13:58:11 +0200

    modified:   dist/assets/css/main/main.css modified:   dist/assets/css/shared/shared.css renamed:    dist/assets/js/main/bundle.js -> dist/assets/js/main.bundle.js renamed:    dist/assets/js/packages/bundle.js -> dist/assets/js/packages.bundle.js modified:   dist/index.html modified:   dist/packages/index.html modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    'main/bundle': './src/main/index.js',
-    'packages/bundle': './src/packages/index.js'
+    'main': './src/main/index.js',
+    'packages': './src/packages/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist/assets/js'),
-    filename: '[name].js'
+    filename: '[name].bundle.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
